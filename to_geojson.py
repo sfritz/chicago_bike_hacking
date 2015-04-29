d36a8ab3dcb27c2988e359a1b89b2a04a82c0aa5
sfritz <scottgfritz@gmail.com>
2015-04-28 22:45:36 -0500

    Output geojson to a file

--- to_geojson.py
+++ to_geojson.py
@@ -1,23 +1,24 @@
-import csv 
+import csv
 import json
 
 geojson = {
 	'type': 'FeatureCollection',
 }
 features = []
-csvfile = open('BL_Current_Active.csv', 'rb')
-reader = csv.DictReader(csvfile)
-for row in reader:
-	f = {
-		'type': 'feature',
-		'properties': row,
-		'geometry': {
-			'type': 'Point',
-			'coordinates': [ row['Longitude'], row['Latitude'] ]
+with open('BL_Current_Active.csv', 'rb') as csvfile:
+	reader = csv.DictReader(csvfile)
+	for row in reader:
+		f = {
+			'type': 'feature',
+			'properties': row,
+			'geometry': {
+				'type': 'Point',
+				'coordinates': [ row['Longitude'], row['Latitude'] ]
+			}
 		}
-	}
-	features.append(f)
-
+		features.append(f)
 
 geojson['features'] = features
-print json.dumps(geojson)
+with open('active_business_licenses.2015-04-28.geojson', 'w') as geojsonfile:
+	geojsonfile.write(json.dumps(geojson))
+
