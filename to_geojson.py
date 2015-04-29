34ecab56c34a7e7cd44d4aae1946673bb1d5dc33
sfritz <scottgfritz@gmail.com>
2015-04-28 23:04:29 -0500

    Handle records with missing lat/long

--- to_geojson.py
+++ to_geojson.py
@@ -5,20 +5,32 @@ geojson = {
 	'type': 'FeatureCollection',
 }
 features = []
+missing = 0
 with open('BL_Current_Active.csv', 'rb') as csvfile:
 	reader = csv.DictReader(csvfile)
 	for row in reader:
-		f = {
-			'type': 'feature',
-			'properties': row,
-			'geometry': {
-				'type': 'Point',
-				'coordinates': [ row['Longitude'], row['Latitude'] ]
+		lat = row['Latitude']
+		lon = row['Longitude']
+		if lat and lon:
+			lat = float(row['Latitude'])
+			lon = float(row['Longitude'])
+			del row['Latitude']
+			del row['Longitude']
+			f = {
+				'type': 'Feature',
+				'properties': row,
+				'geometry': {
+					'type': 'Point',
+					'coordinates': [ lon, lat ]
+				}
 			}
-		}
-		features.append(f)
+			features.append(f)
+		else:
+			missing+=1
 
+print "{} records missing lat/long data".format(missing)
 geojson['features'] = features
 with open('active_business_licenses.2015-04-28.geojson', 'w') as geojsonfile:
 	geojsonfile.write(json.dumps(geojson))
+print "file created"
 
